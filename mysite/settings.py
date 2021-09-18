ab8a598c8733470a7073f06aed93e1b029c46e5f
Pritam <pritam.pritam176@gmail.com>
2021-09-18 15:24:44 +0400

    db

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcrakg61fft3u7',
+        'USER': 'aivcrhctnnjczd',
+        'PASSWORD': '8be0415f11d207a6c8eec4a6878d2490fa5381ce02bd39535395fdb616fb5b11',
+        'HOST': 'ec2-18-215-44-132.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
