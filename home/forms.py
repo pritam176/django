cf25df0bf30f65c243f71158d1e98058f822659a
Pritam <pritam.pritam176@gmail.com>
2021-09-18 16:08:12 +0400

    VS

--- home/forms.py
+++ home/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class CatalogueForm(forms.Form):
     Name= forms.CharField(max_length=100)
-    Number= forms.IntegerField()
-    WhatsappNo= forms.IntegerField()
+    Number= forms.CharField(max_length=13)
+    WhatsappNo= forms.CharField(max_length=13)
     EmailD= forms.CharField(max_length=50)
     Desc=forms.CharField( widget=forms.Textarea )
