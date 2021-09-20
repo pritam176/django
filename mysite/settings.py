cadeb95c920c3a340bf7e7bd724f9a43890ecc44
Pritam <pritam.pritam176@gmail.com>
2021-09-20 10:10:53 +0400

    srsd

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR,'assets')
 MEDIA_ROOT =  os.path.join(BASE_DIR,'media')
 MEDIA_URL  = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
