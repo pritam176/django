1c67531c387cd9f63b8c45f2d99f6c89b6d2e6ff
Pritam <pritam.pritam176@gmail.com>
2021-09-21 11:04:52 +0400

    fsf

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,11 @@ from django.urls import path
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
